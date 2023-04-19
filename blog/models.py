5594b4b27662b2dff49e6c20e30da18156bd4c40
daniel2005erm <iliaz1986kg1986@gmail.com>
2023-04-19 19:40:59 +0600

    dz6

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,22 @@ class Post(models.Model):
     status = models.BooleanField(default=True, verbose_name="Статус публикации")
     created = models.DateTimeField(auto_now=True, verbose_name="Дата создания публикации")
 
-    def __str__(self):
+    def str(self):
         return self.title
+    
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+
+
+class Comment(models.Model):
+    username = models.CharField(max_length=16, verbose_name="Имя автора")
+    text = models.CharField(max_length=300, verbose_name="Текст комментария")
+    created = models.DateTimeField(auto_now=True, verbose_name="Дата создания комментария")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_comment", verbose_name="Пост")
+
+    def str(self):
+        return f"{self.username} - {self.post.title}"
 
     class Meta:
         verbose_name = "Запись"
